81d7bd756fa178e6df3a55f25dca12e90b7a07a3
Joaquin Diaz <joaquin.diaz@ies.cl>
2016-04-25 13:43:02 -0300

    update the orionjs:materialize package

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'specik:orionjs-admin-materialize',
   summary: 'Load Materialize only in the OrionJS admin.',
-  version: '1.0.0',
+  version: '1.0.1',
   git: 'https://github.com/specik/orionjs-admin-materialize'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.use(['meteor-platform', 'orionjs:materialize@1.4.0']);
+  api.use(['meteor-platform', 'orionjs:materialize@1.8.0']);
   api.addFiles(['init.js', 'materialize.min.js', 'templates.html'], 'client');
 });
